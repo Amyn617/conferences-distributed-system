1327adc370045e3c8778e8972371df3db905c7fd
Amyn617 <aarfaoui617@gmail.com>
2025-10-20 10:27:21 +0100

    Create Keynote microservice repository and config

---
+++ keynote-service/src/main/java/ma/enset/keynoteservice/repositories/KeynoteRepository.java
@@ -0,0 +1,10 @@
+package ma.enset.keynoteservice.repositories;
+
+import ma.enset.keynoteservice.entities.Keynote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface KeynoteRepository extends JpaRepository<Keynote, Long> {
+    Boolean checkIfEmailExists(String email);
+}
