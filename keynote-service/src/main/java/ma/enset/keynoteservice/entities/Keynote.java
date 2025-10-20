c2608b17917fa9e46eb7d413a3ee0bdc566da176
Amyn617 <aarfaoui617@gmail.com>
2025-10-20 10:21:57 +0100

    Create Keynote microservice entity

---
+++ keynote-service/src/main/java/ma/enset/keynoteservice/entities/Keynote.java
@@ -0,0 +1,19 @@
+package ma.enset.keynoteservice.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Keynote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String fonction;
+}
