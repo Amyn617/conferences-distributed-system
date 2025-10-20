292b6749a8754a3236a9398625c1bd40f02b22d6
Amyn617 <aarfaoui617@gmail.com>
2025-10-20 09:54:26 +0100

    first commit

---
+++ src/main/java/ma/enset/conferencesmanagement/ConferencesManagementApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.conferencesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferencesManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferencesManagementApplication.class, args);
+    }
+
+}
